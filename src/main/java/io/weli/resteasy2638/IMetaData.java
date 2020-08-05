766aa486d6b7a451cb0700725a3c16ed3d6b663a
阿男 <l.weinan@gmail.com>
2020-08-05 20:58:09 +0800

    poc

--- src/main/java/io/weli/resteasy2638/IMetaData.java
+++ src/main/java/io/weli/resteasy2638/IMetaData.java
@@ -1,5 +1,7 @@
 package io.weli.resteasy2638;
 
+import org.jboss.resteasy.plugins.providers.jsonb.DeserializeAs;
+
 @DeserializeAs(MetaData.class)
 public interface IMetaData {
     String getMetaInfo();
